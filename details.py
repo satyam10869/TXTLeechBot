8d81b6418b53abbb4e8ef299a36e40a0b4d6df21
sumit10869 <142525697+sumit10869@users.noreply.github.com>
2024-06-16 13:11:28 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 22439323
-api_hash = "e0e203c8be2c2c58b04d0c59fc82f507"
-bot_token = "6769775461:AAHKX7yauyaBxnClEzOE3PC94S5wtCg2Gm8"
+api_id = 21513517
+api_hash = "838d3451485b95722878921877f12066"
+bot_token = "7189377174:AAFAy9mdlVhabsidVOPkhLRf9zmBLsfns0w"
 
