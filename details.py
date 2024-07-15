a88b26f59a7af6a035508ed97da8328be5761581
satyam10869 <133392730+satyam10869@users.noreply.github.com>
2024-07-15 06:34:13 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 21513517
-api_hash = "838d3451485b95722878921877f12066"
-bot_token = "7189377174:AAFAy9mdlVhabsidVOPkhLRf9zmBLsfns0w"
+api_id = 7328902365
+api_hash = "e0e203c8be2c2c58b04d0c59fc82f507"
+bot_token = "7027698696:AAFsRd9A3nwQwfQWCBUu3iYGsUPNP1RWLco"
 
